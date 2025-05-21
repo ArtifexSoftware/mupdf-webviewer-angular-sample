259ab3dde66999a85fcfb018ae8fdc09525451ad
Yonghyun Kim <yonghyun.kim@epapyrus.com>
2025-05-21 10:30:22 +0900

    feat: write basic sample
    * set mupdf-webviewer as Angular asset

--- README.md
+++ README.md
@@ -10,7 +10,23 @@ An example of [MuPDF WebViewer](https://webviewer.mupdf.com) within an Angular p
 ## Before you start
 
 - Visit [https://webviewer.mupdf.com/account](https://webviewer.mupdf.com/account) to get a license key.
-- Copy the license key and ...
+- Copy the license key and replace `YOUR_LICENSE_KEY` in `src/app/app.component.ts` with the license key.
 
 > [!TIP]  
 > Ensure to register `localhost` as a domain for your license key if you require to test locally!
+
+## Install
+```shell
+npm ci
+```
+
+## Run
+```shell
+npm run start
+```
+
+## Build
+
+```shell
+npm run build
+```
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component } from '@angular/core';
+import { initMuPDFWebViewer } from 'mupdf-webviewer';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'mupdf-webviewer-angular-sample';
+
+  ngAfterViewInit(): void {
+    from(initMuPDFWebViewer(
+      '#viewer',
+      '/sample.pdf',
+      {
+        libraryPath: 'lib',
+        licenseKey: 'YOUR_LICENSE_KEY',
+      },
+    ))
+      .subscribe({
+        next: mupdf => {
+          /* Use API of MuPDF WebViewer to do something */
+          mupdf.toast.show({ type: 'success', content: 'Document opened' });
+          console.log('MuPDF WebViewer has successfully loaded.');
+        }
+      });
+  }
 }
